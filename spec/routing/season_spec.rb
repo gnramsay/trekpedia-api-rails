5fed0fed54b10c2015bc168f14750cedc7082164
Grant Ramsay <grant@gnramsay.com>
2022-04-18 18:58:08 +0000

    add missing route test for Seasons

--- spec/routing/season_spec.rb
+++ spec/routing/season_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe 'Routes /season' do
   it 'routes to season#index' do
     expect(get('/season')).to route_to('season#index')
   end
+
+  it 'routes to season#show' do
+    expect(get('/season/1')).to route_to('season#show', id: '1')
+  end
 end

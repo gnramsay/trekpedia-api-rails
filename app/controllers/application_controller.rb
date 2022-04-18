4fc5ce560a1250a4b149aab1367e650f697215d4
Grant Ramsay <grant@gnramsay.com>
2022-04-18 19:57:22 +0000

    add a app-wide 404 error response
    overwrite the standard HTML response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :missing_record
+
+  def missing_record
+    error = {
+      status: 404,
+      title: 'Record not Found',
+      detail: 'That record does not exist in the database'
+    }
+    render json: { errors: [error] }, status: 404
+  end
 end

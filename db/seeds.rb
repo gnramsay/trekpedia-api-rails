23ae4c37fcd50b91454d5ee35bd14db5f49a5be3
Grant Ramsay <grant@gnramsay.com>
2022-04-19 18:13:09 +0000

    refactor seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,6 +26,22 @@ def create_season(series, season_num, season_data)
   )
 end
 
+def reset_sequences
+  # ActiveRecord::Base.connection.tables.each do |table|
+  %w[seasons series].each do |table|
+    result = begin
+      ActiveRecord::Base.connection.execute("SELECT id FROM #{table} ORDER BY id DESC LIMIT 1")
+    rescue StandardError
+      (puts "Warning: not procesing table #{table}. Id is missing?"
+
+       next)
+    end
+    ai_val = result.any? ? result.first['id'].to_i + 1 : 1
+    puts "  -> Resetting auto increment ID for #{table} to #{ai_val}".light_red
+    ActiveRecord::Base.connection.execute("ALTER SEQUENCE #{table}_id_seq RESTART WITH #{ai_val}")
+  end
+end
+
 puts "\nPopulating Trekpedia data base from local JSON files".light_blue
 
 series_list = JSON.parse(File.read(Rails.root.join('trekdata', 'output', 'star_trek_series_info.json')))
@@ -34,19 +50,7 @@ puts "\nClearing out existing data from the database.".red
 Season.destroy_all
 Series.destroy_all
 
-# ActiveRecord::Base.connection.tables.each do |table|
-%w[seasons series].each do |table|
-  result = begin
-    ActiveRecord::Base.connection.execute("SELECT id FROM #{table} ORDER BY id DESC LIMIT 1")
-  rescue StandardError
-    (puts "Warning: not procesing table #{table}. Id is missing?"
-
-     next)
-  end
-  ai_val = result.any? ? result.first['id'].to_i + 1 : 1
-  puts "  -> Resetting auto increment ID for #{table} to #{ai_val}".light_red
-  ActiveRecord::Base.connection.execute("ALTER SEQUENCE #{table}_id_seq RESTART WITH #{ai_val}")
-end
+reset_sequences
 
 puts "\n"
 series_list.each do |series|

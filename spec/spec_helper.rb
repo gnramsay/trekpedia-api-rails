82e66ed54103175c191885fbe80773a995f4f617
Grant Ramsay <grant@gnramsay.com>
2022-04-20 17:18:38 +0000

    add filters for coverage
    Dont check coverage for channels, jobs and mailers,
    we will not be using these in this API (yet ...)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@
 require 'simplecov'
 SimpleCov.start 'rails' do
   enable_coverage :branch
+  add_filter ['/channels/', '/jobs/', '/mailers/']
 end
 
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
